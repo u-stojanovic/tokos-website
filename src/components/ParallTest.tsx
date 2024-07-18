5b3e15838bb6142100f04ec0776770a2fca76d57
Boris Nikolic <borisnikolic2302@gmail.com>
2024-07-18 15:33:15 +0200

    Parallax effect implemented

--- src/components/ParallTest.tsx
+++ src/components/ParallTest.tsx
@@ -1,13 +1,7 @@
-"use client";
-
-import { Parallax } from "react-scroll-parallax";
-
 export default function Test() {
   return (
-    <Parallax speed={-20}>
-      <div className="flex justify-center w-screen h-screen text-center items-center bg-red-500">
-        Test
-      </div>
-    </Parallax>
+    <div className="flex justify-center w-full h-screen text-center items-center bg-red-500">
+      Test
+    </div>
   );
 }
