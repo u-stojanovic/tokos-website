bf8f85fe6619e9c49366da324ad8f90e43ad8885
Boris Nikolic <borisnikolic2302@gmail.com>
2024-07-22 19:46:31 +0200

    Navbar fixed

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -11,7 +11,7 @@ import {
 } from "../ui/navigation-menu";
 import { ModeToggle } from "./ThemeModeToggle";
 import BlurFade from "../magicui/blur-fade";
-import { XIcon } from "lucide-react";
+import { XIcon, Menu } from "lucide-react";
 
 const BLUR_FADE_DELAY = 0.04;
 const navLinkStyle =
@@ -30,7 +30,11 @@ export default function Navbar() {
   return (
     <header>
       <nav
-        className={`fixed top-0 left-0 w-full z-50 ${isMenuOpen ? "bg-gray-100 dark:bg-gray-900 lg:bg-transparent" : "bg-gradient-to-b from-zinc-500 to-transparent dark:from-gray-900"} backdrop-filter backdrop-blur-sm text-white`}
+        className={`fixed top-0 left-0 w-full z-50 ${
+          isMenuOpen
+            ? "bg-gray-100 dark:bg-gray-900 lg:bg-transparent"
+            : "bg-gradient-to-b from-zinc-500 to-transparent dark:from-gray-900"
+        } backdrop-filter backdrop-blur-sm text-white`}
       >
         <BlurFade delay={BLUR_FADE_DELAY}>
           <div className="w-full flex justify-between items-center">
@@ -62,10 +66,9 @@ export function Logo() {
 
 function DesktopMenu() {
   return (
-    <div className="hidden md:flex justify-between gap-4 items-center">
+    <div className="hidden lg:flex justify-between gap-4 items-center">
       <div className="flex flex-row gap-11 mr-12 items-center">
         <NavLinks />
-        <ModeToggle />
       </div>
       <CartIcon />
     </div>
@@ -82,10 +85,10 @@ function NavLinks() {
             <NavigationMenuTrigger>Prodavnica</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid gap-3 p-4 text-nowrap">
-                <NavListItem href="/cakes" text="Torte" />
-                <NavListItem href="/cookies" text="Kolači" />
-                <NavListItem href="/treats" text="Poslastice" />
-                <NavListItem href="/salty-catering" text="Slani Ketering" />
+                <NavListItem href="/torte" text="Torte" />
+                <NavListItem href="/kolaci" text="Kolači" />
+                <NavListItem href="/poslastice" text="Poslastice" />
+                <NavListItem href="/slani-ketering" text="Slani Ketering" />
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
@@ -130,52 +133,41 @@ function NavListItem({ href, text, onClick }: NavListItemProps) {
 
 function CartIcon() {
   return (
-    <Link href="/korpa">
-      <div className="ml-6 mr-3 relative">
-        <span className="bg-pink-300 text-white rounded-full absolute w-4 h-4 -top-1 -right-1 flex justify-center items-center text-sm text-center">
-          12
-        </span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6 hover:text-pink-200 transition duration-300 text-gray-900 dark:text-white"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-          />
-        </svg>
-      </div>
-    </Link>
+    <div className="flex flex-row justify-center items-center content-center">
+      <ModeToggle />
+      <Link href="/korpa">
+        <div className="ml-6 mr-3 relative">
+          <span className="bg-pink-300 text-white rounded-full absolute w-4 h-4 -top-1 -right-1 flex justify-center items-center text-sm text-center">
+            12
+          </span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 hover:text-pink-200 transition duration-300 text-gray-900 dark:text-white"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+          </svg>
+        </div>
+      </Link>
+    </div>
   );
 }
 
 function MobileMenuToggle({ isMenuOpen, setIsMenuOpen }: MobileMenuProps) {
   return (
     <div className="flex items-center gap-6 lg:hidden">
-      <ModeToggle />
       <button className="ml-4 p-2" onClick={() => setIsMenuOpen(!isMenuOpen)}>
         {isMenuOpen ? (
-          <XIcon />
+          <XIcon className="hover:text-pink-200" />
         ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-gray-900 dark:text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
+          <Menu className="hover:text-pink-200" />
         )}
       </button>
       <CartIcon />
@@ -185,7 +177,7 @@ function MobileMenuToggle({ isMenuOpen, setIsMenuOpen }: MobileMenuProps) {
 
 function MobileMenu({ setIsMenuOpen }: MobileMenuProps) {
   return (
-    <div className="fixed top-0 left-0 font-bold w-full h-fit bg-gray-100 dark:bg-gray-900 flex flex-col items-center gap-6 py-4 z-10 lg:hidden">
+    <div className="fixed font-bold w-full h-fit bg-gray-100 dark:bg-gray-900 flex flex-col items-center gap-6 py-4 z-10 lg:hidden">
       <NavLink href="/" text="Naslovna" onClick={() => setIsMenuOpen(false)} />
       <NavigationMenu>
         <NavigationMenuList>
