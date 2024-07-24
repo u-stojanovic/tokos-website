68fd461465f51c86e6544fff3795be405c53d4ad
GooseGodX <uros.stojanovic99@gmail.com>
2024-07-24 17:38:35 +0200

    build: faq section

--- src/app/(info)/faq/page.tsx
+++ src/app/(info)/faq/page.tsx
@@ -2,35 +2,60 @@
 import React from "react";
 import { FaChevronDown, FaChevronUp, FaQuestionCircle } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function FAQ() {
   const [activeIndex, setActiveIndex] = React.useState(null);
 
   const faqItems = [
     {
-      question: "What is Insightify?",
+      question:
+        "Koliko unapred treba da naručim proizvode iz vaše poslastičarnice?",
       answer:
-        "Insightify is an AI-powered optimization tool that analyzes your website's design, content, and user experience to uncover hidden opportunities and drive higher conversions.",
+        "Najbolje je da narudžbine izvršite najmanje dva dana unapred kako bismo mogli da osiguramo da svi proizvodi budu sveži i spremni na vreme.",
     },
     {
-      question: "How does Insightify work?",
-      answer:
-        "Insightify works by using advanced algorithms to analyze various aspects of your website, including design elements, content quality, and user interaction patterns, to provide actionable insights and recommendations.",
+      question: "Koji su načini poručivanja proizvoda iz vaše poslastičarnice?",
+      answer: (
+        <div>
+          Proizvode možete poručiti na sledeće načine:
+          <ul className="list-disc ml-5">
+            <li>Online: Preko našeg sajta.</li>
+            <li>
+              Telefonom: Pozivom na broj{" "}
+              <Link href="tel:+381654274270" className="text-blue-500">
+                +381 (65) 427-427-0
+              </Link>
+            </li>
+            <li>
+              Instagram: Slanjem poruke na naš profil{" "}
+              <Link
+                href="https://www.instagram.com/slatkakucatokos/"
+                className="text-blue-500"
+                target="_blank"
+              >
+                @slatkakucatokos
+              </Link>
+            </li>
+            <li>
+              Facebook: Slanjem poruke na naš profil{" "}
+              <Link
+                href="https://www.facebook.com/slatkakuca.tokos"
+                className="text-blue-500"
+                target="_blank"
+              >
+                Slatka Kuća Tokos
+              </Link>
+              .
+            </li>
+            <li>WhatsApp i Viber: Korišćenjem aplikacija WhatsApp i Viber.</li>
+          </ul>
+        </div>
+      ),
     },
     {
-      question: "Can I try Insightify for free?",
-      answer:
-        "Yes, Insightify offers free improvements for users. Each user gets 2 free improvements every 6 hours.",
-    },
-    {
-      question: "Who can use?",
-      answer:
-        "Insightify is designed to work with a wide range of websites, including e-commerce sites, blogs, corporate websites, and more.",
-    },
-    {
-      question: "How to get started?",
-      answer:
-        "To get started with Insightify, simply click on the 'Get Started' button and follow the instructions to set up your account and start optimizing your website.",
+      question: "Koji su načini plaćanja?",
+      answer: "keš keš keš",
     },
   ];
 
@@ -41,19 +66,20 @@ export default function FAQ() {
   return (
     <section
       id="faq"
-      className="w-full py-12 md:py-24 lg:py-32 bg-gray-50 dark:bg-gray-900"
+      className="w-full py-12 md:py-24 lg:py-32 bg-lightMode-surface dark:bg-darkMode-surface"
     >
       <div className="container px-4 md:px-6">
         <div className="flex flex-col p-6 items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
-            <div className="inline-block rounded-lg bg-blue-200 px-3 py-1 text-sm dark:bg-blue-700 text-blue-900 dark:text-blue-100">
+            <div className="inline-block rounded-lg bg-lightMode-primary px-3 py-1 text-sm dark:bg-darkMode-primary text-lightMode-text">
               FAQ
             </div>
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-gray-900 dark:text-gray-50">
-              Frequently Asked Questions
+              Često postavljana pitanja
             </h2>
             <p className="max-w-[900px] text-gray-500 md:text-xl lg:text-base xl:text-xl dark:text-gray-400">
-              Here are some of the most common questions about Insightify.
+              Ovde su neka od najčešće postavljanih pitanja u vezi naše web
+              stranice.
             </p>
           </div>
         </div>
@@ -63,17 +89,17 @@ export default function FAQ() {
               key={index}
               className={`mb-4 p-6 rounded-lg shadow-md transition-transform transform ${
                 activeIndex === index
-                  ? "bg-blue-100 dark:bg-blue-800"
-                  : "bg-white dark:bg-gray-800"
+                  ? "bg-lightMode-background dark:bg-darkMode-background"
+                  : "bg-lightMode-background dark:bg-darkMode-background"
               }`}
             >
               <Button
-                variant="ghost"
-                className="w-full flex justify-between items-center text-left focus:outline-none"
+                variant="default"
+                className="w-full flex justify-between items-center text-left focus:outline-none focus:bg-none dark:hover:bg-none"
                 onClick={() => toggleFAQ(index)}
               >
-                <span className="flex items-center text-lg font-medium text-gray-900 dark:text-gray-50 break-words">
-                  <FaQuestionCircle className="mr-2 text-blue-600 dark:text-blue-300" />
+                <span className="flex items-center text-lg font-medium text-lightMode-text dark:text-gray-50 break-words">
+                  <FaQuestionCircle className="mr-2 text-lightMode-primary dark:text-darkMode-primary" />
                   {item.question}
                 </span>
                 <span className="text-gray-500 dark:text-gray-400">
@@ -81,7 +107,7 @@ export default function FAQ() {
                 </span>
               </Button>
               {activeIndex === index && (
-                <div className="mt-4 text-gray-700 dark:text-gray-300 break-words">
+                <div className="mt-4 text-lightMode-text dark:text-darkMode-text break-words">
                   {item.answer}
                 </div>
               )}
--- src/app/(info)/layout.tsx
+++ src/app/(info)/layout.tsx
@@ -1,12 +1,14 @@
+import Footer from "@/components/shared/Landing/Footer";
 import Navbar from "@/components/shared/Navbar";
 
 export default function InfoLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex">
+    <div className="flex flex-col">
       <Navbar />
       <div className="flex-1">{children}</div>
+      <Footer />
     </div>
   );
 }
