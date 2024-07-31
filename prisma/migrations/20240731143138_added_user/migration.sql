d295a85d0cd19ccb0ad98e78e592a6c6f5ef5e6c
Boris Nikolic <borisnikolic2302@gmail.com>
2024-07-31 18:57:45 +0200

    New Migrations

---
+++ prisma/migrations/20240731143138_added_user/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "UserRoles" AS ENUM ('HeadAdmin', 'Worker', 'None');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "role" "UserRoles" NOT NULL DEFAULT 'None',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_email_key" ON "User"("id", "email");
