a0cd55de5d56fbdf3b1a38add0e615de3764ba0d
Boris Nikolic <borisnikolic2302@gmail.com>
2024-08-22 21:06:03 +0200

    fix: link to svi proizvodi fixed in categories section

--- src/data/categories.tsx
+++ src/data/categories.tsx
@@ -14,7 +14,7 @@ export const CategoriesData = [
       "https://images.unsplash.com/photo-1447078806655-40579c2520d6?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     // count: 120,
     icon: <PieChartIcon className="w-8 h-8" />,
-    link: "/products/category/svi-proizvodi",
+    link: "/products/svi-proizvodi",
   },
   {
     title: "Torte",
