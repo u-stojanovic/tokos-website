ee16122a96b92069665a4ca76e536afe39a69c1f
Boris Nikolic <borisnikolic2302@gmail.com>
2024-09-09 20:55:12 +0200

    Added prefetching in home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
-"use client";
-
-import { Parallax } from "react-scroll-parallax";
-import ShopInfoSection from "@/components/shared/Landing/Sections/ShopInfoSection";
-import CategoriesInfoSection from "@/components/shared/Landing/Sections/CategoriesSection";
-import Navbar from "@/components/shared/Navbar";
-import Carousel from "@/components/shared/Landing/Carousel/Carousel";
+import ParallaxCarousel from "@/components/shared/HomeComponents/ParallaxCarousel";
+import ParallaxCategories from "@/components/shared/HomeComponents/ParallaxCategories";
 import Footer from "@/components/shared/Landing/Footer";
+import ShopInfoSection from "@/components/shared/Landing/Sections/ShopInfoSection";
+import Navbar from "@/components/shared/Navbar";
+import { getAllProducts } from "@/lib/actions/productActions";
+import { QueryClient } from "@tanstack/react-query";
 
 export default function Home() {
+  const queryClient = new QueryClient();
+  queryClient.prefetchQuery({
+    queryKey: ["svi-proizvodi", 1],
+    queryFn: ({ queryKey }) => getAllProducts(queryKey[1] as number),
+  });
+
   return (
     <div className="flex flex-col">
       <Navbar />
       <div className="flex-grow">
-        <Parallax speed={-20} className="relative">
-          <Carousel />
-        </Parallax>
+        <ParallaxCarousel />
         <div className="relative z-20">
           <ShopInfoSection />
         </div>
-        <Parallax>
-          <div className="flex items-start">
-            <CategoriesInfoSection />
-          </div>
-        </Parallax>
+        <ParallaxCategories />
       </div>
       <div className="relative z-50 h-fit">
         <Footer />
--- src/app/products/[id]/ProductDetailsSkeleton.tsx
+++ src/app/products/[id]/ProductDetailsSkeleton.tsx
@@ -5,17 +5,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, EffectFade, Thumbs } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
 import "swiper/css/effect-fade";
 import "swiper/css/thumbs";
 
 export default function ProductDetailsSkeleton() {
   return (
-    <div className="max-w-7xl mx-auto py-10 px-6">
+    <div className="max-w-7xl mx-auto py-10 px-4 sm:px-6">
       <div className="flex items-center mb-4">
-        <div className="w-32 h-6 bg-gray-300 dark:bg-gray-700 animate-pulse"></div>
+        <div className="w-24 h-6 bg-gray-300 dark:bg-gray-700 animate-pulse"></div>
       </div>
-      <div className="grid md:grid-cols-2 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10">
         <div className="space-y-6">
           {/* Main Image Swiper Skeleton */}
           <Swiper
@@ -28,27 +27,27 @@ export default function ProductDetailsSkeleton() {
             className="w-full h-auto"
           >
             <SwiperSlide className="flex justify-center items-center">
-              <div className="w-full h-96 bg-gray-300 dark:bg-gray-700 animate-pulse rounded-lg"></div>
+              <div className="w-full h-72 md:h-96 bg-gray-300 dark:bg-gray-700 animate-pulse rounded-lg"></div>
             </SwiperSlide>
           </Swiper>
 
           {/* Thumbnail Swiper Skeleton */}
           <Swiper
             spaceBetween={10}
-            slidesPerView={4}
+            slidesPerView={3} // Reduced the number of visible thumbnails for mobile
             freeMode
             watchSlidesProgress
             className="w-full h-auto"
           >
             {[...Array(4)].map((_, index) => (
               <SwiperSlide key={index} className="cursor-pointer">
-                <div className="w-full h-24 bg-gray-300 dark:bg-gray-700 animate-pulse rounded-lg"></div>
+                <div className="w-full h-20 bg-gray-300 dark:bg-gray-700 animate-pulse rounded-lg"></div>
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
 
-        <div className="space-y-6">
+        <div className="space-y-4 md:space-y-6">
           {/* Title Skeleton */}
           <div className="w-3/4 h-8 bg-gray-300 dark:bg-gray-700 animate-pulse"></div>
 
@@ -60,7 +59,7 @@ export default function ProductDetailsSkeleton() {
           </div>
 
           {/* Price Skeleton */}
-          <div className="w-1/4 h-8 bg-gray-300 dark:bg-gray-700 animate-pulse"></div>
+          <div className="w-1/3 h-8 bg-gray-300 dark:bg-gray-700 animate-pulse"></div>
 
           {/* Category Skeleton */}
           <div className="space-y-2">
